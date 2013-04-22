a8ccb26bf842462bc3a13156a9ea995017fe3798
陈彦 <hudiezizai82@163.com>
2013-04-23 01:56:34 +0800

    Centos环境配置

---
+++ 练习/cheny/Union/src/com/example/union/NewInfo.java
@@ -0,0 +1,12 @@
+package com.example.union;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewInfo extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+}
